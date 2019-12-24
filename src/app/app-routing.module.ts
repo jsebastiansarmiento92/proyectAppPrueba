4b0291ff38d666ec48880e1005153f4e50996acf
Sebastian <jsebastiansarmiento92@gmail.com>
2019-12-23 20:15:25 -0500

    se añaden rutas al nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {PrivatePageComponent} from './components/private-page/private-page.component';
+import {NfPageComponent} from './components/nf-page/nf-page.component';
+
+//import {AuthGuard} from './guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'privado', component: PrivatePageComponent},
+  {path: '**', component: NfPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NfPageComponent } from './components/nf-page/nf-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PrivatePageComponent } from './components/private-page/private-page.com
     NavBarComponent,
     RegisterComponent,
     LoginComponent,
-    PrivatePageComponent
+    PrivatePageComponent,
+    NfPageComponent
   ],
   imports: [
     BrowserModule,
